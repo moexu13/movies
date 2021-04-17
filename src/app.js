d004287829c0c042058c1ee73ada5155952a68f2
Melissa Albarella <moexu13@gmail.com>
2021-04-17 16:40:02 -0700

    added logging

--- src/app.js
+++ src/app.js
@@ -1,5 +1,22 @@
-if (process.env.USER) require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
+const logger = require("./config/logger");
+
+const errorHandler = require("./errors/errorHandler");
+const notFound = require("./errors/notFound");
+const moviesRouter = require("./movies/movies.router");
+const theatersRouter = require("./theaters/theaters.router");
+
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(logger);
+
+app.use("/movies", moviesRouter);
+app.use("/theaters", theatersRouter);
+
+app.use(notFound);
+app.use(errorHandler);
 
 module.exports = app;
